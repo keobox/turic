de6d6eddc2a663a197aa04d1e26f2fae20b91508
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-06-14 14:30:29 +0200

    Lesson 5 size of a tring literal

--- lesson05/string.c
+++ lesson05/string.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-    char* str2 = "hello";
+    char str2[] = "hello";
     printf("Hello world %s\n", str2);
+    printf("sizeof: %lu\n", sizeof(str2));
     return 0;
 }
