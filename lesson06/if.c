2a7fd734c5beea0fce84d8fbe1ca5bbd519cae5d
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-06-23 17:04:13 +0200

    Lesson 6 one line blocks style

--- lesson06/if.c
+++ lesson06/if.c
@@ -3,9 +3,7 @@
 int main(void) {
     int i = 5;
 
-    if (i > 3)
-        printf("i > 3\n");
-    else
-        printf("i <= 3\n");
+    if (i > 3) printf("i > 3\n");
+    else printf("i <= 3\n");
     return 0;
 }
