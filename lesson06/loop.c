9c11e1dd389a3af85f4b36d165d31e2ffe00e45a
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-06-23 17:51:34 +0200

    Lesson 6 if and goto and while equivalence

--- lesson06/loop.c
+++ lesson06/loop.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
+    int i = 0;
 
-    l10: printf("hello\n");
-    l20: goto l10;
+// Next line is a "label" and starts with a letter
+again:
+    printf("%d\n", i);
+    i++;
+    if (i < 10) goto again;
 
+    printf("\n");
+    i = 0;
+    while(i < 10) {
+        printf("%d\n", i);
+        i++;
+    }
     return 0;
 }
