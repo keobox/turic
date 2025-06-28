d2f773ae3cc8e0c049e46e98dafa192f459f8185
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-06-28 11:17:11 +0200

    Lesson 7 skip the initialization part in the for loop

--- lesson07/loops.c
+++ lesson07/loops.c
@@ -2,12 +2,7 @@
 
 int main (void) {
     int i = 0;
-    while (i < 10) {
-        printf("%d\n", i);
-        i++;
-    }
-    printf("\n");
-    for (i = 0; i < 10; i++) {
+    for (; i < 10; i++) {
         printf("%d\n", i);
     }
     return 0;
