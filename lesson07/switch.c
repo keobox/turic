8bd1f17e8631bb2ae088bcb39aa9047518ba2c9f
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-06-28 12:28:09 +0200

    Lesson 7 switch

---
+++ lesson07/switch.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main (void) {
+    int i = 5;
+
+    switch(i) {
+        case 5:
+            printf("Is a five\n");
+            printf("It's really a five\n");
+            break;
+        case 7:
+            printf("Is a seven\n");
+            break;
+        default:
+            printf("Is another number\n");
+            break;
+    }
+
+    printf("\n");
+    // is equivalent to if
+    if (i == 5) {
+        printf("Is a five\n");
+        printf("It's really a five\n");
+    } else if (i == 7) {
+        printf("Is a seven\n");
+    } else {
+        printf("Is another number\n");
+    }
+    return 0;
+}
