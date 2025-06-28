3822975123ba2975d338df5009a38ff8443559a5
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-06-28 10:56:06 +0200

    Lesson 7 tail call optimization

--- lesson07/recursion.c
+++ lesson07/recursion.c
@@ -6,7 +6,18 @@ void count(int start, int end) {
     count(start+1, end);
 }
 
+void count_tail_call_optimized(int start, int end) {
+iterate:
+    if (start > end) return;
+    printf("%d\n", start);
+    start = start + 1;
+    goto iterate;
+}
+
+
 int main(void) {
     count(0, 9);
+    printf("\n");
+    count_tail_call_optimized(0,9);
     return 0;
 }
