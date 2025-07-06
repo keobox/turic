a49c5a9d868f21eae519d47a1e8c5fd4d5b26d4a
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-07-06 14:14:22 +0200

    Lesson 8 print_grid and set_grid

--- lesson08/life.c
+++ lesson08/life.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-#define GRID_COLS 25
-#define GRID_ROWS 25
+#define GRID_COLS 20
+#define GRID_ROWS 20
 #define ALIVE '*'
 #define DEAD '.'
 
@@ -20,23 +20,54 @@ int cell_to_index(int x, int y) {
     }
     if (x >= GRID_COLS) x = x % GRID_COLS;
     if (y >= GRID_ROWS) y = y % GRID_ROWS;
-
-    return 0;
+    return y*GRID_COLS + x;
 }
 
 /* This function sets the specified cell at x,y to the specified state */
-void setcell(char* grid, int x, int y, int state) {
+void set_cell(char* grid, int x, int y, char state) {
     grid[cell_to_index(x, y)] = state;
 }
 
 /* This function returns the state of the grid at x,y */
-char getcell(char* grid, int x, int y) {
+char get_cell(char* grid, int x, int y) {
     return grid[cell_to_index(x, y)];
+}
+
+/* Show the grid on the screen, clearing the terminal using the required
+   VT100 escape sequence */
+void print_grid(char* grid) {
+    for (int y = 0; y < GRID_ROWS; y++) {
+        for (int x = 0; x < GRID_COLS; x++) {
+            printf("%c", get_cell(grid, x, y));
+        }
+        printf("\n");
+    }
+}
+
+/* Set all the grid cells to the specified state.
+*/
+void set_grid(char* grid, char state) {
+    for (int y = 0; y < GRID_ROWS; y++) {
+        for (int x = 0; x < GRID_COLS; x++) {
+            set_cell(grid, x, y, state);
+        }
+    }
 }
 
 int main(void) {
     char grid[GRID_COLS * GRID_ROWS];
-    setcell(grid, 10, 10, ALIVE);
-
+    set_grid(grid, DEAD);
+    set_cell(grid, 10, 10, ALIVE);
+    /* BEGIN test wrapping */
+    set_cell(grid, 19, 10, ALIVE);
+    set_cell(grid, 20, 10, ALIVE);
+    set_cell(grid, 40, 11, ALIVE);
+    set_cell(grid, 41, 11, ALIVE);
+    set_cell(grid, -1, 12, ALIVE);
+    set_cell(grid, -19, 12, ALIVE);
+    set_cell(grid, -20, 12, ALIVE);
+    set_cell(grid, -21, 13, ALIVE);
+    /* END   test wrapping */
+    print_grid(grid);
     return 0;
 }
