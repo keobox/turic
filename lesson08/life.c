ce7f6edcb01fc2784014fd52de8548737b5954d8
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-07-06 11:21:58 +0200

    Lesson 8 setcell function

--- lesson08/life.c
+++ lesson08/life.c
@@ -5,8 +5,14 @@
 #define ALIVE '*'
 #define DEAD '.'
 
+/* This function sets the specified cell at x,y to the specified state */
+void setcell(char* grid, int x, int y, int state) {
+    grid[y * GRID_COLS + x] = state;
+}
+
 int main(void) {
-    char grid[Cols*Rows];
+    char grid[GRID_COLS * GRID_ROWS];
+    setcell(grid, 10, 10, ALIVE);
 
     return 0;
 }
