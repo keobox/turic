c9be2e54b5cd3bc4014dfdb260801631928be844
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-07-13 17:53:21 +0200

    Lesson 9 declare a pointer variable

--- lesson09/ptr1.c
+++ lesson09/ptr1.c
@@ -2,6 +2,8 @@
 
 int main(void) {
     int x = 5;
+    int *y = NULL; // same as int *y=0
     printf("%d\n", x);
+    printf("%p\n", y);
     return 0;
 }
--- lesson09/topics.md
+++ lesson09/topics.md
@@ -1,2 +1,5 @@
 # Topics
 
+* Declare a point to int using a style more compliant with C syntax: put the `*` near the variable,
+not near the type.
+
