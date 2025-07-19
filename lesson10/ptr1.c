18c873ada543ef2f5ff1db5067071f9902b20b6f
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-07-19 13:29:50 +0200

    Lesson 10 array / pointer equivalence

--- lesson10/ptr1.c
+++ lesson10/ptr1.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    int x = 5;
-    int *y = &x;
-    int **z = &y;
-    printf("x is stored in %p and y is stored in %p\n", y, z);
-    printf("%d, %d, %d\n", (int) sizeof(x), (int) sizeof(y), (int) sizeof(z));
+    char mystr[] = "Hello World";
+    char *p = mystr;
+    printf("At %p I can see: %s\n", mystr, mystr);
+    printf("At %p I can see: %s\n", p, mystr);
     return 0;
 }
