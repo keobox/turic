92ea84744f94feeda78ed725dc027259dca0eee7
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-07-19 14:37:26 +0200

    Lesson 10 write your printf

--- lesson10/ptr1.c
+++ lesson10/ptr1.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
-int main(void) {
-    char mystr[] = "AABBCCDDEEFF";
-    char *p =  mystr;
-    printf("At the beginning p is %p\n", p);
+void printstr(char *p) {
     while (*p != 0) {
         putchar(*p);
         p++;
     }
     printf("\n");
-    printf("At the end p is %p\n", p);
+}
+
+int main(void) {
+    char mystr[] = "AABBCCDDEEFF";
+    char *p =  mystr;
+    printstr(p);
+    printstr(mystr);
     return 0;
 }
