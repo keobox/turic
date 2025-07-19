9342a06ad4db3e22289b1f7c2f186e3bbe842ff5
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-07-19 14:13:44 +0200

    Lesson 10 another demo of pointers math using a short pointer

--- lesson10/ptr1.c
+++ lesson10/ptr1.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
 int main(void) {
-    char mystr[] = "Hello World";
-    char *p = mystr;
-    printf("%c%c\n", p[0], p[1]);
-    printf("%c%c\n", *p, *(p+1));
+    char mystr[] = "AABBCCDDEEFF";
+    short *p = (short*) mystr;
+    /* Since asc of 'A' is 65 this will print
+       65 + 65 * 256 the decimal value of the
+       first 2 bytes "AA" */
+    printf("%d\n", *p);
+
+    p++;
+    /* The pointer increment above will
+    increment the address by sizeof(short), the
+    type of the pointer, so p in now pointing to BB
+    and since asc 'B' is 66, the printf will print
+    66 + 66 * 256 */
+    printf("%d\n", *p);
+
+    p = (short*) mystr;
+    /* Will print "EE" in decimal = 17733 */
+    printf("%d\n", p[4]);
     return 0;
 }
