5cb51740c3d887fb489e869ae86907d12f3113bb
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-07-19 13:19:50 +0200

    Lesson 10 size of pointers demo

--- lesson10/ptr1.c
+++ lesson10/ptr1.c
@@ -5,5 +5,6 @@ int main(void) {
     int *y = &x;
     int **z = &y;
     printf("x is stored in %p and y is stored in %p\n", y, z);
+    printf("%d, %d, %d\n", (int) sizeof(x), (int) sizeof(y), (int) sizeof(z));
     return 0;
 }
