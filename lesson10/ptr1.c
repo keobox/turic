e6e14cca874df26f2ec7de316f6dfe26d3f19b2b
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-07-19 12:54:21 +0200

    Lesson 10 clarify ampersend operator

--- lesson10/ptr1.c
+++ lesson10/ptr1.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 
-void incr(int *p) {
-    printf("Before incr %d\n", *p);
-    p[0] = p[0] + 1;
-}
-
 int main(void) {
     int x = 5;
-    printf("x was %d\n", x);
-    incr(&x);
-    printf("x is %d\n", x);
+    int *y = &x;
+    printf("x is stored in %p and the value is %d\n", y, x);
+    printf("x is stored in %p and the value is %d\n", &x, x);
     return 0;
 }
