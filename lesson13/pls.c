e0e3cf8afc68386b7508d183281d12764a291b89
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-08-09 15:41:30 +0200

    Lesson 13 PLS string with max length of 4 bytes so 2^32 so 4GB

--- lesson13/pls.c
+++ lesson13/pls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 /* pls.c Prefixed Length String */
 
@@ -7,52 +8,51 @@
  * string in 'init' of length 'len'.
  *
  * The created string has the following layout:
- * +-+-----------\\\
- * |L|My string here
- * +-+-----------\\\
+ * +-+---------------\\\
+ * |LLLL|My string here
+ * +-+---------------\\\
  *
- * Where L is one unsigned byte stating the total length of the string.
- * Thus this string are binary safe: bytes of value 0 are permitted in
+ * Where LLLL are four unsigned bytes containing the total length of the string.
+ * Thus these strings are binary safe: bytes of value 0 are permitted in
  * the middle of the string.
  *
  * Warning: this function does not check for buffer overflow.
  */
 
-char *ps_create(char *init, int len) {
-    char *s = malloc(1 + len + 1);
-    unsigned char *lenptr = (unsigned char *) s;
-    // write the length in the first byte
+char *ps_create(char *init, uint32_t len) {
+    char *s = malloc(4 + len + 1);
+    uint32_t *lenptr = (uint32_t *) s;
+    // write the length in the first 4 bytes
     *lenptr = len;
+    s += 4;
     // write the rest
-    for (int j = 0; j < len ; j++) {
-        s[j+1] = init[j];
+    for (uint32_t j = 0; j < len ; j++) {
+        s[j] = init[j]; // FIXME use memcopy
     }
-    s[len + 1] = 0;
+    s[len] = 0;
     return s;
-}
-
-/* Returns the pointer to the NULL terminated c-string embedded
-   inside or PS 's' string.
- */
-char *ps_getc(char *s) {
-    return s + 1;
 }
 
 /* Display the string s on the screen.
  */
 void ps_print(char *s) {
-    unsigned char *lenptr = (unsigned char *) s;
-    for (int j = 0; j < *lenptr; j++) {
-        putchar(s[j+1]);
+    uint32_t *lenptr = (uint32_t *) (s-4);
+    for (uint32_t j = 0; j < *lenptr; j++) {
+        putchar(s[j]);
     }
     printf("\n");
 }
 
+/* Dealloc a prefixed length string */
+void ps_free(char *s) {
+    free(s-4);
+}
+
 int main(void) {
-    char *mystr = ps_create("Hello World", 11);
+    char *mystr = ps_create("Hello WorldHello WorldHello World", 33);
     ps_print(mystr);
     ps_print(mystr);
-    printf("%s\n", ps_getc(mystr));
-    free(mystr);
+    printf("%s\n", mystr);
+    ps_free(mystr);
     return 0;
 }
--- lesson13/topics.md
+++ lesson13/topics.md
@@ -2,3 +2,6 @@
 
 * Recap of prefixed strings and interoperalability with c-strings.
   * Iteroperability function `ps_getc`.
+* Increment the length size to 4 bytes.
+  * `#include <stdint.h>` includes integer type like `uint32_t` `int32_t` etc.
+  * In this case the max length is 2^32 = 4GB.
