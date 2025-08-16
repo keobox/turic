86facb54e33f139a2f8400d3bc00228ac61f9be4
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-08-16 15:40:59 +0200

    Lesson 14 add basic function for handling fractions

--- lesson14/fract.c
+++ lesson14/fract.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void set_fraction(int *f, int num, int dem) {
+    f[0] = num;
+    f[1] = dem;
+}
+
+void print_fraction(int *f) {
+    printf("%d/%d\n", f[0], f[1]);
+}
+
 int main(void) {
     int *f = malloc(sizeof(int)*2);
-    /* 1111|2222 */
-    // f[0] = numerator ;
-    // f[0] = denominator ;
+    set_fraction(f, 1, 2);
+    print_fraction(f);
     return 0;
 }
--- lesson14/topics.md
+++ lesson14/topics.md
@@ -5,3 +5,4 @@
   * Cliffhanger recap: use a struct as Prefixed Length header: see `struct.c`.
 * To review C structs write a program for fractions handling.
 * To make room for 2 ints numerator and denominator I could use a `malloc`.
+* Add basic function for frations handling.
