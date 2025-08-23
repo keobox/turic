fd7bbb2b52ce477161b638e13bc0ed2936404837
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-08-23 17:58:17 +0200

    Lesson 15 introduce FILE and fopen

--- lesson15/tac.c
+++ lesson15/tac.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    for (int i = 0; i < argc; i++) {
-        printf("%d: %s\n", i, argv[i]);
+    if (argc != 2) {
+        printf("Missing file name\n");
+        return 1;
     }
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        printf("File does not exists\n");
+        return 1;
+    }
+
+    fclose(fp);
     return 0;
 }
--- lesson15/topics.md
+++ lesson15/topics.md
@@ -3,3 +3,5 @@
 * Recap of what done in lesson 14 about `fract.c`.
 * Intention: implement the `tac` command that prints the reverse of `cat` command.
 * Explain program `args`.
+* Explain the `FILE` C API: file pointers and `fopen`.
+  * `man fopen`.
