902b8a31429cab6749441b34f5e7d3968b9ae740
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-08-23 18:37:42 +0200

    Lesson 15 implement the tac command

--- lesson15/tac.c
+++ lesson15/tac.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct line {
+    char *s;
+    struct line *next;
+};
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -12,8 +19,24 @@ int main(int argc, char **argv) {
     }
 
     char buf[1024];
+    struct line *head = NULL;
     while(fgets(buf, sizeof(buf), fp) != NULL) {
-        printf("%s", buf);
+        /* the malloc return value should be checked for NULL:
+           memory error */
+        struct line *l = malloc(sizeof(struct line));
+        size_t linelen = strlen(buf);
+        l->s = malloc(linelen + 1);
+        /* the string copy can be done with memcpy */
+        for(size_t j = 0; j <= linelen; j++) {
+            l->s[j] = buf[j];
+        }
+        l->next = head;
+        head = l;
+    }
+
+    while(head != NULL) {
+        printf("%s", head->s);
+        head = head->next;
     }
 
     fclose(fp);
--- lesson15/topics.md
+++ lesson15/topics.md
@@ -6,3 +6,8 @@
 * Explain the `FILE` C API: file pointers and `fopen`.
   * `man fopen`.
 * Explain the `fgets` function for reading the lines of a file.
+* Write a `Linked List` of strings struct to print the strings of a file
+in reverse order.
+* A last comment: in this case we wrote a `Linked List` abstraction and
+a O(N) algorithm to print in reverse but using a O(N) memory where
+N is the lines number.
