45c09edef14ba347542fe62ed7ed2da371cfa10d
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-09-06 21:02:36 +0200

    Lesson 17 use a char array in a struct

--- lesson17/issue1.c
+++ lesson17/issue1.c
@@ -1,24 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 struct pls {
     long len;
-    char *str;
+    char str[20];
 };
 
+#define HEXDUMP_CHARS_PER_LINE 16
 void hexdump(void *p, size_t len) {
    unsigned char *byte = p;
+   size_t po = 0;
    for (size_t j = 0; j < len; j++) {
        printf("%02X ", byte[j]);
+       if ((j+1) % HEXDUMP_CHARS_PER_LINE == 0 || j == len-1) {
+           printf("\t");
+           for (size_t i = po; i <= j ; i++) {
+               /* isprint = is_printable */
+               char c = isprint((char)byte[i]) ? byte[i] : '.';
+               printf("%c", c);
+           }
+           printf("\n");
+           po = j+1;
+       }
    }
-   printf("\n");
 }
 
 int main(void) {
     struct pls s;
+
+    /* memset(&s, 0xFF, sizeof(s)); */
     s.len = 10;
-    s.str = malloc(10+1);
     memcpy(s.str, "1234567890", 11);
     printf("%p\n", s.str);
     hexdump(&s, sizeof(s));
--- lesson17/topics.md
+++ lesson17/topics.md
@@ -2,3 +2,8 @@
 
 * Use this lesson to solve some doubts arose in Lesson 16.
 * Explain what is `char *str` in a struct: is a pointer to a string.
+* If I write `str[20]` now C consider this, in the struct, as an
+integer followed by 20 chars. No pointer is present. So, in this case,
+there's no equivalence among `char *s` and `char s[]` like in other
+contexts.
+* Complete the `hexdump` function using the ternary operator.
