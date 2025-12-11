b4ec193d72ba1c76814550cbab592d572f6b370d
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-12-11 10:26:23 +0100

    Lesson 28 vsnprintf usage for a 'lowercase' printf version

--- lesson28/vararg.c
+++ lesson28/vararg.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <ctype.h>
 
 void foo(const char* fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
-    vprintf(fmt, ap);
+    char mybuf[32];
+    vsnprintf(mybuf, sizeof(mybuf), fmt, ap);
+    for (int j = 0; j < sizeof(mybuf); j++) {
+        mybuf[j] = tolower(mybuf[j]);
+    }
+
+    printf("%s\n", mybuf);
     va_end(ap);
 }
 
 int main(void) {
     printf("Hello world\n");
-    foo("%d %d %s %d", 10, 20, "ciao ciao", 5);
+    foo("Hi Hi hi AAAABBB %d %d %s", 10, 20, "CIAO");
     return 0;
 }
